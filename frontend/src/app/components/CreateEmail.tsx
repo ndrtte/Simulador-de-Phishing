434b7078e5fd3c31b6d42c68db3c0e169556bdfa
ndrtte <andreamorelaleman@gmail.com>
2026-03-28 19:57:07 -0600

    Perdon ahi se me olvido lo del asunto

--- frontend/src/app/components/CreateEmail.tsx
+++ frontend/src/app/components/CreateEmail.tsx
@@ -133,7 +133,7 @@ export function CreateEmail() {
                 htmlFor="subject"
                 className="block text-sm font-medium text-gray-700 mb-2"
               >
-                
+                Asunto
               </label>
               <input
                 type="text"
